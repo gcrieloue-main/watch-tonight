2723fa4cd679c08c1062a2e75b1da51f2a34864b
Crieloue Gilles <gilles.crieloue@gmail.com>
2023-08-28 23:00:24 +0200

    first commit

---
+++ server.js
@@ -0,0 +1,234 @@
+const TorrentSearchApi = require("torrent-search-api");
+TorrentSearchApi.enableProvider("ThePirateBay");
+
+const express = require("express");
+const fs = require("fs");
+const _ = require("lodash");
+const app = express();
+const port = 3001;
+
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
+
+async function getTmdbMovies(
+  page,
+  options = {
+    genre: 27,
+    type: "none",
+  }
+) {
+  console.log(page, options);
+  const pageId = page || 1;
+  let url;
+  switch (options.type) {
+    case "now_playing":
+      url = `https://api.themoviedb.org/3/movie/now_playing?page=${pageId}`;
+      break;
+    case "upcoming":
+      url = `https://api.themoviedb.org/3/movie/upcoming?page=${pageId}`;
+      break;
+    case "popular":
+      url = `https://api.themoviedb.org/3/movie/popular?page=${pageId}`;
+      break;
+    default:
+      url = `https://api.themoviedb.org/3/discover/movie?with_genres=${options.genre}&vote_average.gte=6&vote_count.gte=10&sort_by=primary_release_date.desc&page=${pageId}`;
+  }
+
+  console.log(url);
+  const data = await (
+    await fetch(url, {
+      headers: {
+        Accept: "application/json",
+        Authorization:
+          "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIzMWRjOWVhZWY2OTZlNjRhZDYyNjYwYjI1NjBhYjdmYyIsInN1YiI6IjY0ZWE0MjdkNTk0Yzk0MDBhY2IwOGFkYiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.91n344WTAJ3PahMIVEBcj1aqE6BQGMBgaXleFtQL2wQ",
+      },
+    })
+  ).json();
+  //  console.log(data)
+  return data;
+}
+
+async function getTmdbMovieDetails(id) {
+  return await (
+    await fetch(`https://api.themoviedb.org/3/movie/${id}?language=en-US`, {
+      headers: {
+        Accept: "application/json",
+        Authorization:
+          "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIzMWRjOWVhZWY2OTZlNjRhZDYyNjYwYjI1NjBhYjdmYyIsInN1YiI6IjY0ZWE0MjdkNTk0Yzk0MDBhY2IwOGFkYiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.91n344WTAJ3PahMIVEBcj1aqE6BQGMBgaXleFtQL2wQ",
+      },
+    })
+  ).json();
+}
+
+const memoGetTmdbMovieDetails = _.memoize(getTmdbMovieDetails);
+
+async function getOmdbMovieDetails(title) {
+  // const movies =  await (await fetch('http://www.omdbapi.com/?apikey=f33929a7&t=star+wars')).json()
+  // console.log(movies);
+  const data = await (
+    await fetch(`http://www.omdbapi.com/?apikey=f33929a7&t=` + title, {
+      headers: {
+        Accept: "application/json",
+        Authorization:
+          "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIzMWRjOWVhZWY2OTZlNjRhZDYyNjYwYjI1NjBhYjdmYyIsInN1YiI6IjY0ZWE0MjdkNTk0Yzk0MDBhY2IwOGFkYiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.91n344WTAJ3PahMIVEBcj1aqE6BQGMBgaXleFtQL2wQ",
+      },
+    })
+  ).json();
+  // console.log(data)
+  return data;
+}
+
+const memoGetOmdbMovieDetails = _.memoize(getOmdbMovieDetails);
+
+async function getTorrentDetails(title) {
+  if (!title) {
+    return;
+  }
+  const torrents = await TorrentSearchApi.search(title, "Video", 1);
+  return torrents[0];
+}
+
+const memoGetTorrentDetails = _.memoize(getTorrentDetails);
+
+async function getData(page, options) {
+  console.log("# Query data...");
+
+  const data = await getTmdbMovies(page, options);
+
+  const resultsWithDetails = await Promise.all(
+    data.results?.map(async (data) => {
+      return await addTmdbMovieDetail(data);
+    })
+  );
+
+  return { ...data, results: resultsWithDetails };
+}
+
+const memoGetData = _.memoize(getData, (...args) => JSON.stringify(args));
+
+async function addTmdbMovieDetail(data) {
+  return {
+    details: await memoGetTmdbMovieDetails(data.id),
+    omdbDetails: await memoGetOmdbMovieDetails(data.original_title),
+    torrentDetails: await memoGetTorrentDetails(data.original_title),
+  };
+}
+
+function loadWatchedMoviesIds() {
+  const watched = fs.readFileSync("./watch.txt", "utf8");
+  return watched.split("\n").filter((id) => id != "");
+}
+
+async function loadWatchedMovies(ids) {
+  console.log("# Query Watched Movies...");
+  return {
+    results: await Promise.all(
+      ids.map(async (id) => {
+        const detail = await memoGetTmdbMovieDetails(id);
+        return addTmdbMovieDetail(detail);
+      })
+    ),
+  };
+}
+
+memoGetData(1);
+
+app.get("/movies", async (req, res) => {
+  res.send(await memoGetData(1));
+});
+
+app.get("/movies/:page", async function (req, res) {
+  const movies = await memoGetData(+req.params.page);
+  res.send(movies);
+  memoGetData(+req.params.page + 1);
+});
+
+app.get("/movies/popular", async (req, res) => {
+  res.send(await memoGetData(1, { type: "popular" }));
+});
+
+app.get("/movies/popular/:page", async (req, res) => {
+  const movies = await memoGetData(+req.params.page, { type: "popular" });
+  res.send(movies);
+  memoGetData(+req.params.page + 1, { type: "popular" });
+});
+
+app.get("/movies/upcoming", async (req, res) => {
+  res.send(await memoGetData(1, { type: "upcoming" }));
+});
+
+app.get("/movies/upcoming/:page", async (req, res) => {
+  const movies = await memoGetData(+req.params.page, { type: "upcoming" });
+  res.send(movies);
+  memoGetData(+req.params.page + 1, { type: "upcoming" });
+});
+
+app.get("/movies/now_playing", async (req, res) => {
+  res.send(await memoGetData(1, { type: "now_playing" }));
+});
+
+app.get("/movies/now_playing/:page", async (req, res) => {
+  const movies = await memoGetData(+req.params.page, { type: "now_playing" });
+  res.send(movies);
+  memoGetData(+req.params.page + 1, { type: "now_playing" });
+});
+
+app.get("/watch/:id", async function (req, res) {
+  const id = req.params.id;
+  const alreadyWatched = await loadWatchedMoviesIds();
+  if (alreadyWatched.includes(id)) {
+    res.send(id + " already added");
+    return;
+  }
+
+  fs.appendFile("./watch.txt", id + "\n", (err) => {
+    if (err) {
+      console.error(err);
+    }
+    // fichier écrit avec succès
+  });
+  res.send(alreadyWatched.concat([id]));
+});
+
+app.get("/watch/delete/:id", async function (req, res) {
+  const id = req.params.id;
+  const alreadyWatched = await loadWatchedMoviesIds();
+  if (!alreadyWatched.includes(id)) {
+    res.send(alreadyWatched);
+    return;
+  }
+
+  fs.writeFile(
+    "./watch.txt",
+    alreadyWatched.filter((existingId) => existingId != id).join("\n"),
+    (err) => {
+      if (err) {
+        console.error(err);
+      }
+      // fichier écrit avec succès
+    }
+  );
+
+  res.send(alreadyWatched.filter((existingId) => existingId != id));
+});
+
+app.get("/watchedIds", async function (req, res) {
+  const data = await loadWatchedMoviesIds();
+  // console.log(data);
+  res.send(data);
+});
+
+app.get("/watched", async function (req, res) {
+  const watchedIds = await loadWatchedMoviesIds();
+  res.send(await loadWatchedMovies(watchedIds));
+});
+
+app.listen(port, () => {
+  console.log(`Movie DB on port ${port}`);
+});
--- src/App.js
+++ src/App.js
@@ -1,23 +1,230 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+
+import { useEffect, useState } from "react";
 
 function App() {
+  const [movies, setMovies] = useState([]);
+  const [watchedIds, setWatchedIds] = useState([]);
+  const [page, setPage] = useState(1);
+  const [category, setCategory] = useState("popular");
+  const [isLoading, setIsLoading] = useState(false);
+
+  let title;
+  switch (category) {
+    case "popular":
+      title = "Popular Movies";
+      break;
+    case "watched":
+      title = "Watch list";
+      break;
+    case "now_playing":
+      title = "Now playing";
+      break;
+    case "upcoming":
+      title = "Upcoming";
+      break;
+    default:
+      title = "Horror Movies";
+  }
+
+  useEffect(() => {
+    setPage(1);
+  }, [category]);
+
+  useEffect(() => {
+    const controller = new AbortController();
+    const signal = controller.signal;
+
+    let url;
+    if (category === "watched") {
+      url = "http://localhost:3001/watched";
+    } else if (category === "popular") {
+      url = "http://localhost:3001/movies/popular/" + page;
+    } else if (category === "upcoming") {
+      url = "http://localhost:3001/movies/upcoming/" + page;
+    } else if (category === "now_playing") {
+      url = "http://localhost:3001/movies/now_playing/" + page;
+    } else {
+      url = "http://localhost:3001/movies/" + page;
+    }
+    setIsLoading(true);
+
+    fetch(url, {
+      signal: signal,
+    })
+      .catch((error) => {
+        if (error.name !== "AbortError") {
+          console.log("error is", error);
+        }
+      })
+      .then(async (result) => {
+        const m = await result;
+        if (!m) return;
+        const movies = await m.json();
+        setMovies(movies);
+        setIsLoading(false);
+      });
+
+    return () => {
+      controller.abort();
+    };
+  }, [page, category]);
+
+  useEffect(() => {
+    if (category === "watched") {
+      fetch("http://localhost:3001/watched").then(async (result) => {
+        const m = await result;
+        setMovies(await m.json());
+      });
+    }
+  }, [watchedIds]);
+
+  useEffect(() => {
+    fetch("http://localhost:3001/watchedIds").then(async (result) => {
+      const m = await result;
+      setWatchedIds(await m.json());
+    });
+  }, []);
+
+  function next() {
+    setPage((currentPage) => currentPage + 1);
+  }
+
+  function previous() {
+    setPage((currentPage) => currentPage - 1);
+  }
+
+  function addWatchdId(id) {
+    if (watchedIds.includes("" + id)) {
+      return;
+    }
+
+    fetch(`http://localhost:3001/watch/${id}`).then(async (result) => {
+      const m = await result;
+      setWatchedIds((currentWatchIds) =>
+        setWatchedIds(currentWatchIds.concat("" + id))
+      );
+    });
+  }
+
+  function removeWatchdId(id) {
+    if (!watchedIds.includes("" + id)) {
+      return;
+    }
+
+    fetch(`http://localhost:3001/watch/delete/${id}`).then(async (result) => {
+      const m = await result;
+      setWatchedIds((currentWatchIds) =>
+        setWatchedIds(currentWatchIds.filter((current) => current != id))
+      );
+    });
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ul className="menu">
+        <li onClick={() => setCategory("watched")}>Watch list</li>
+        <li onClick={() => setCategory("upcoming")}>Upcoming</li>
+        <li onClick={() => setCategory("now_playing")}>Now playing</li>
+        <li onClick={() => setCategory("popular")}>Popular movies</li>
+        <li onClick={() => setCategory("horror")}>Horror movies</li>
+      </ul>
+      <h1>{title}</h1>
+      {/* <pre>Watched ids : {JSON.stringify(watchedIds)}</pre> */}
+      {/* <pre>{JSON.stringify(movies?.results?.[0], null, 2)}</pre> */}
+      <div className={"movies" + (isLoading ? " loading" : " ")}>
+        {category == "watched" && !(movies?.results?.length > 0) && (
+          <div>
+            <p>No watched movie !</p>
+          </div>
+        )}
+        {movies?.results
+          ?.filter((result) => result.details.status_code != 34)
+          ?.map((result) => (
+            <div className="movie" key={result.details.original_title}>
+              <h2>{result.details.original_title}</h2>
+              <h3>{result.details.release_date}</h3>
+              <div className="poster">
+                {!watchedIds?.includes("" + result.details.id) && (
+                  <span
+                    className="watch_action watch__add"
+                    onClick={() => addWatchdId(result.details.id)}
+                  >
+                    +
+                  </span>
+                )}
+                {watchedIds?.includes("" + result.details.id) && (
+                  <span
+                    className="watch_action watch__add"
+                    onClick={() => removeWatchdId(result.details.id)}
+                  >
+                    -
+                  </span>
+                )}
+                {result.omdbDetails.imdbRating >= 6 &&
+                  result.omdbDetails.imdbRating < 7 && (
+                    <span className="approved">✓</span>
+                  )}
+                {result.omdbDetails.imdbRating >= 7 && (
+                  <span className="approved approved_plus">✓+</span>
+                )}
+                <img
+                  className={
+                    result.omdbDetails.imdbRating < 5 ? "grayscale" : ""
+                  }
+                  loading="lazy"
+                  src={
+                    "https://www.themoviedb.org/t/p/w300_and_h450_bestv2" +
+                    result.details.poster_path
+                  }
+                />
+              </div>
+              <div className="rating">
+                <ul>
+                  {result.details.imdb_id && (
+                    <li>
+                      <a
+                        href={
+                          "https://www.imdb.com/title/" +
+                          result.details.imdb_id +
+                          "/"
+                        }
+                      >
+                        IMDB
+                      </a>
+                      <span>
+                        {" "}
+                        :{" "}
+                        <strong>
+                          {result.omdbDetails.imdbRating || "N/A"}
+                        </strong>{" "}
+                      </span>
+                    </li>
+                  )}
+                  <li>
+                    TMDB : {result.details.vote_average} (
+                    {result.details.vote_count})
+                  </li>
+                </ul>
+              </div>
+              <p className="download">
+                {result.torrentDetails?.seeds > 0 && (
+                  <a href={result.torrentDetails.magnet}>
+                    Download {result.torrentDetails.title} (
+                    {result.torrentDetails.size})
+                  </a>
+                )}
+              </p>
+            </div>
+          ))}
+      </div>
+      {category != "watched" && (
+        <div className="buttons">
+          {page > 1 && <button onClick={previous}>PREVIOUS</button>}
+          {page > 1 && page}
+          <button onClick={next}>NEXT</button>
+        </div>
+      )}
     </div>
   );
 }
