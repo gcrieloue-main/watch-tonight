11eba4d22b6bcc3249fa1bc2bd818ace7a6ffd29
Crieloue Gilles <gilles.crieloue@gmail.com>
2024-01-11 14:44:11 +0100

    'improve'

--- app/page.js
+++ app/page.js
@@ -1,113 +1,151 @@
-import Image from "next/image";
+"use client";
 
-export default function Home() {
+import { useFetch } from "use-http";
+import { useEffect, useState, useRef } from "react";
+import { Spinner } from "@nextui-org/react";
+import { Pagination } from "./pagination";
+import { Menu } from "./menu";
+import { Movie } from "./movie";
+import autoAnimate from "@formkit/auto-animate";
+import classNames from "classnames";
+
+const API_URL = "/api";
+
+function App() {
+  const [movies, setMovies] = useState([]);
+  const [watchedIds, setWatchedIds] = useState([]);
+  const [page, setPage] = useState(1);
+  const [category, setCategory] = useState("now_playing");
+  const [isLoading, setIsLoading] = useState(false);
+  const { get, response } = useFetch({ data: [] });
+  const parent = useRef(null);
+
+  const CATEGORY_WATCHED = "watched";
+
+  const loadMovies = async (url) => {
+    const loadingTimeout = setTimeout(() => {
+      setIsLoading(true);
+    }, 300);
+    const result = await get(url);
+    if (response.ok) setMovies(result);
+    clearTimeout(loadingTimeout);
+    setTimeout(() => {
+      window.scrollTo(0, 0);
+      setIsLoading(false);
+    }, 250); // scroll to top after autoanimate
+  };
+
+  const loadWatchedIds = async () => {
+    const url = `${API_URL}/watchedIds`;
+    const result = await get(url);
+    if (response.ok) setWatchedIds(result);
+  };
+
+  useEffect(() => {
+    parent.current && autoAnimate(parent.current);
+  }, [parent]);
+
+  useEffect(() => {
+    setPage(1);
+  }, [category]);
+
+  useEffect(() => {
+    if (category === CATEGORY_WATCHED) {
+      return;
+    }
+
+    const urls = {
+      watched: "watched",
+      popular: "movies/popular/" + page,
+      upcoming: "movies/upcoming/" + page,
+      now_playing: "movies/now_playing/" + page,
+      default: "movies/" + page,
+    };
+    loadMovies(`${API_URL}/${urls[category] || urls["default"]}`);
+  }, [page, category]);
+
+  useEffect(() => {
+    if (category === CATEGORY_WATCHED) {
+      console.log("load watched movies");
+      const url = `${API_URL}/watched?ids=${JSON.stringify(watchedIds)}`;
+      loadMovies(url);
+    }
+  }, [category, watchedIds]);
+
+  useEffect(() => {
+    loadWatchedIds();
+  }, []);
+
+  function next() {
+    setPage((currentPage) => currentPage + 1);
+  }
+
+  function previous() {
+    setPage((currentPage) => currentPage - 1);
+  }
+
+  function addWatchdId(id) {
+    if (watchedIds?.includes(`${id}`)) {
+      console.warn(`${id} already in watchedIds`, watchedIds);
+      return;
+    }
+    fetch(`${API_URL}/watch/${id}`).then(async () => {
+      setWatchedIds((currentWatchIds) => currentWatchIds.concat(`${id}`));
+    });
+  }
+
+  function removeWatchdId(id) {
+    if (!watchedIds?.includes(`${id}`)) {
+      console.warn(`${id} already absent from watchedIds`, watchedIds);
+      return;
+    }
+    fetch(`${API_URL}/watch/delete/${id}`).then(async () => {
+      setWatchedIds((currentWatchIds) =>
+        currentWatchIds.filter((current) => current !== "" + id),
+      );
+    });
+  }
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <div className="App">
+      {isLoading && (
+        <div className="spinner">
+          <Spinner size="lg" />
         </div>
+      )}
+      <Menu setCategory={setCategory} category={category} />
+      <div
+        className={classNames("movies", { loading: isLoading })}
+        ref={parent}
+      >
+        {category === CATEGORY_WATCHED && !(movies?.results?.length > 0) && (
+          <div>
+            <p>No watched movie !</p>
+          </div>
+        )}
+        {movies?.results
+          ?.filter((result) => result.details.status_code !== 34)
+          ?.map((result) => (
+            <Movie
+              key={result.details.title}
+              result={result}
+              watchedIds={watchedIds}
+              addWatchId={addWatchdId}
+              removeWatchId={removeWatchdId}
+            />
+          ))}
       </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
+      {category !== CATEGORY_WATCHED && (
+        <Pagination
+          category={category}
+          page={page}
+          previous={previous}
+          next={next}
+          isLoading={isLoading}
         />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+      )}
+    </div>
   );
 }
+
+export default App;
